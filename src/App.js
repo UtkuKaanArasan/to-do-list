a50eeda4e339d40c1a3aee665258e8641fefb88b
UtkuKaanArasan <utkukaan000@protonmail.ch>
2022-05-25 18:55:31 +0300

    Dummy data moved to another file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
 import Nav from './Components/Nav';
 import Todo from './Components/to-do/Todo';
+import { dummyData } from './Data/dummyData';
 
-const dummyData = [
-  {
-    id: 1,
-    title: "Something to do",
-    done: false
-  },
-  {
-    id: 2,
-    title: "Something to do 2",
-    done: false
-  },
-]
 
 function App() {
   return (
---
+++ src/Data/dummyData.js
@@ -0,0 +1,11 @@
+export const dummyData = [{
+    "id": 1,
+    "title": "Something to do",
+    "done": false
+},
+{
+    "id": 2,
+    "title": "Something to do 2",
+    "done": false
+},
+]
